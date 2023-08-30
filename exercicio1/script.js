2b65e39c15231841e200b5f1f1fb4bbce5ea5a50
Gustavo <gustavosoares2790@gmail.com>
2023-08-29 21:00:31 -0300

    Exercícios sobre funções

---
+++ exercicio1/script.js
@@ -0,0 +1,67 @@
+//function lerNome(nome){
+//    console.log(`Olá, ${nome}`)
+//}
+
+//lerNome('Gustavo')
+//lerNome('Alexandre')
+
+const lerNome = (nome)=>{
+    console.log(nome)
+}
+
+lerNome('Gustavo')
+lerNome('Alexandre')
+
+/*
+function lerNumero(){
+    let numero = Number(prompt('Digite um número:'))
+    const numeroX1 = numero * 1
+    console.log(numeroX1)
+    const numeroX2 = numero * 2
+    console.log(numeroX2)
+    const numeroX3 = numero * 3
+    console.log(numeroX3)
+    const numeroX4 = numero * 4
+    console.log(numeroX4)
+    const numeroX5 = numero * 5
+    console.log(numeroX5)
+    const numeroX6 = numero * 6
+    console.log(numeroX6)
+    const numeroX7 = numero * 7
+    console.log(numeroX7)
+    const numeroX8 = numero * 8
+    console.log(numeroX8)
+    const numeroX9 = numero * 9
+    console.log(numeroX9)
+    const numeroX10 = numero * 10
+    console.log(numeroX10)
+}
+
+lerNumero()
+*/
+
+const lerNumero = ()=>{
+    let numero = Number(prompt('Digite um número:'))
+    const numeroX1 = numero * 1
+    console.log('n * 1 =:' , numeroX1)
+    const numeroX2 = numero * 2
+    console.log('n * 2 =:' , numeroX2)
+    const numeroX3 = numero * 3
+    console.log('n * 3 =:' , numeroX3)
+    const numeroX4 = numero * 4
+    console.log('n * 4 =:' , numeroX4)
+    const numeroX5 = numero * 5
+    console.log('n * 5 =:' , numeroX5)
+    const numeroX6 = numero * 6
+    console.log('n * 6 =:' , numeroX6)
+    const numeroX7 = numero * 7
+    console.log('n * 7 =:' , numeroX7)
+    const numeroX8 = numero * 8
+    console.log('n * 8 =:' , numeroX8)
+    const numeroX9 = numero * 9
+    console.log('n * 9 =:' , numeroX9)
+    const numeroX10 = numero * 10
+    console.log('n * 10 =:' , numeroX10)
+}
+
+lerNumero()
